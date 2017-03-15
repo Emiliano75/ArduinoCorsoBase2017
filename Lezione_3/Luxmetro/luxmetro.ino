30dfc2306725865140828c4d5588ea1f9baa2a51
Gruppo Utenti Linux Livorno <gulligle@gmail.com>
2017-03-15 16:33:22 +0100

    formula per calcolo LUX

--- Lezione_3/Luxmetro/luxmetro.ino
+++ Lezione_3/Luxmetro/luxmetro.ino
@@ -15,6 +15,13 @@ __version__ = "1.0"
 #define LDR A0 // Pin Fotoresistenza
 #define RPD 10000 // resistenza pull down della LDR
 
+
+
+// Calcolo valore in Lux dal valore dell resistenza: L=10^(b) * R^(m) dove vale la formula log(R)=m*log(L)+b che approssima il grafico
+// log-log della caratteristica Luminosità-Resistenza della LDR
+#define b 1.544f
+#define m -0.622f
+
 float LDR_Res=0.0; // valore della resistenza della LDR
 float Vin=0.0; // Valore lettura ingresso Analogico
 float Lux=0.0; // Valore in Lux dell'illuminamento
@@ -22,8 +29,8 @@ float Lux=0.0; // Valore in Lux dell'illuminamento
 //calcola il valore in Lux ricavato dalla resistenza della LDR
 float  R2Lux(float Res){
 
-  // TO DO ........
-  return log10(Res);
+  // implemento la funzione  L=10^(b) * R^(m)
+  return pow(10,b)*pow(Res,m);
   
 }
 
@@ -40,7 +47,8 @@ void loop()
     //calcolo il valore in Ohm della LDR
     LDR_Res=(float)(RPD*(1023-Vin)/Vin);
     Lux=R2Lux(LDR_Res);
-    Serial.println(LDR_Res); // Scrivo sul monitor seriale il valore di LDRValue         
+    Serial.print("LUX= "); // Scrivo sul monitor seriale il valore di LDRValue  
+    Serial.println(Lux);
 
     delay(100);
 }
