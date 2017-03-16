cbc5930ed83fae21ba5d85912a372cc9bf601317
Gruppo Utenti Linux Livorno <gulligle@gmail.com>
2017-03-16 10:41:19 +0100

    Corretta formula calcolo LUX

--- Lezione_3/Luxmetro/Luxmetro.ino
+++ Lezione_3/Luxmetro/Luxmetro.ino
@@ -16,9 +16,8 @@ __version__ = "1.0"
 #define RPD 10000 // resistenza pull down della LDR
 
 
-
-// Calcolo valore in Lux dal valore dell resistenza: L=10^(b) * R^(m) dove vale la formula log(R)=m*log(L)+b che approssima il grafico
-// log-log della caratteristica Luminosità-Resistenza della LDR
+// Calcolo valore in Lux dal valore dell resistenza: L=(R*10^(-b))^(1/m) dove vale la formula log(R)=m*log(L)+b che approssima il grafico
+// log-log della caratteristica Luminosità-Resistenza della LDR come da Datasheet
 #define b 1.544f
 #define m -0.622f
 
@@ -28,10 +27,8 @@ float Lux=0.0; // Valore in Lux dell'illuminamento
 
 //calcola il valore in Lux ricavato dalla resistenza della LDR
 float  R2Lux(float Res){
-
-  // implemento la funzione  L=10^(b) * R^(m)
-  return pow(10,b)*pow(Res,m);
-  
+  // implemento la funzione  L=(R*10^(-b))^(1/m)
+  return pow(Res*pow(10,-b),1\m);
 }
 
 
@@ -47,8 +44,11 @@ void loop()
     //calcolo il valore in Ohm della LDR
     LDR_Res=(float)(RPD*(1023-Vin)/Vin);
     Lux=R2Lux(LDR_Res);
+    Serial.print("RES= "); // Scrivo sul monitor seriale il valore di LDRValue  
+    Serial.println(LDR_Res);
     Serial.print("LUX= "); // Scrivo sul monitor seriale il valore di LDRValue  
     Serial.println(Lux);
+    Serial.println("========================");
 
     delay(100);
 }
