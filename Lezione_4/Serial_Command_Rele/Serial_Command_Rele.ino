2a3501ce5a5e4d9aa7eea0cd5ee571239a870afd
Emiliano75 <beviugo2@gmail.com>
2017-03-21 09:23:32 +0100

    Update Serial_Command_Rele.ino

--- Lezione_4/Serial_Command_Rele/Serial_Command_Rele.ino
+++ Lezione_4/Serial_Command_Rele/Serial_Command_Rele.ino
@@ -12,27 +12,30 @@ __version__ = "1.0"
 ***************************************************************************
 */
 
-#define PinRele 2                                                // Definizione del pin per il rele'
+#define PinRele 2                                                   // Definizione del pin per il rele'
 
-void setup() {                                                   // Inizializzo la modalita' del "PinRele" e la comunicazione seriale
+void setup() {                                                      // Inizializzo la modalita' del "PinRele" e la comunicazione seriale
   pinMode(PinRele, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available()) {                                       // Se leggo qualcosa sulla porta seriale...
-    int Serial_command = Serial.read();                            // Scrivo nella variabile "Serial_command" il risultato della lettura seriale
-
-    switch(Serial_command)                                         // L'istruzione "switch" permette diverse possibilita' di utilizzo del codice
+  if(Serial.available()) {                                          // Se leggo qualcosa sulla porta seriale...
+    int Serial_command = Serial.read();                             // Scrivo nella variabile "Serial_command" il risultato della lettura seriale
+ 
+    switch(Serial_command)                                          // L'istruzione "switch" permette diverse possibilita' di utilizzo del codice
     {
-          case '1':                                                // Nel caso il valore della variabile "Serial_command" sia '1'...
-                   Serial.println("Rele' attivato");               // Eseguo delle istruzioni...
+          case '1':                                                 // Nel caso il valore della variabile "Serial_command" sia '1'...
+                   Serial.println("Rele' attivato");                // Eseguo delle istruzioni...
                    digitalWrite(PinRele, HIGH);
                    break;
-          case '2':                                                // Nel caso il valore della variabile "Serial_command" sia '2'...
-                   Serial.println("Rele' disattivato");            // Eseguo delle istruzioni...
+          case '2':                                                 // Nel caso il valore della variabile "Serial_command" sia '2'...
+                   Serial.println("Rele' disattivato");             // Eseguo delle istruzioni...
                    digitalWrite(PinRele, LOW);
                    break;
+          default:                                                   // Nel caso il valore della variabile "Serial_command" non corrisponda a un "case"
+                   Serial.println("Comando non riconosciuto !!!");   // Stampo un messaggio di errore
+                   break;          
     }
   }
 }
