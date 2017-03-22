89f69d1389af83ba879131875264fc056ca0cf83
Gruppo Utenti Linux Livorno <gulligle@gmail.com>
2017-03-22 23:55:30 +0100

    Codice commentato e ripulito

--- Lezione_6/rtc_lcd/rtc_lcd.ino
+++ Lezione_6/rtc_lcd/rtc_lcd.ino
@@ -18,9 +18,11 @@ __version__ = "1.0"
 #include <LiquidCrystal_I2C.h> // Libreria per LCD con interfaccia I2C
 
 #define USE_LCD
-// alimento modulo RTC direttamente dai PIN di I/O di Arduin0
+// alimento modulo RTC direttamente dai PIN di I/O di Arduino
 #define VCC_PIN 8
 #define GND_PIN 9
+
+// collegamenti dati del modulo RTC DS1302
 #define CLK_PIN 10
 #define DAT_PIN 11
 #define RST_PIN 12
@@ -28,10 +30,10 @@ __version__ = "1.0"
 // array di interi per memorizzare i valori per settare l'RTC
 byte dateTime[7]={0,0,0,0,0,0,0};
 
-
-
+#ifdef USE_LCD
+//definisco oggetto lcd del tipo LiquidCrystal_I2C
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
-
+#endif
 
 //Creo oggetto virtuabotixRTC(uint8_t CLK, uint8_t DAT, uint8_t RST);  
 virtuabotixRTC myRTC(CLK_PIN, DAT_PIN, RST_PIN);
@@ -56,61 +58,54 @@ void setup()  {
   lcd.print("RTC+LCD Gulli");   // Stampo il messaggio
   delay(3000);
 #endif
-
-// Set the current date, and time in the following format:
-// seconds, minutes, hours, day of the week, day of the month, month, year
-//myRTC.setDS1302Time(00, 59, 23, 6, 10, 1, 2014);
 }
 
-//=======================================================================================================//|    
-//                                                                                                       //|   
-//                         Printout by accessing Single Element objects BEGIN                            //|   
-//                                                                                                       //|   
-//=======================================================================================================//|   
-//                                                                                                       //|   
-//  This example utilizes the Serial.print function to access individual data elements, this allows for  //|
-//  user defined output format.                                                                          //|
-//                                                                                                       //|   
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//|  
-void loop()  {                                                                                           //| 
-// This allows for the update of variables for time or accessing the individual elements.                //|
-  myRTC.updateTime();                                                                                    //| 
-             
+
+void loop()  {
+// aggiorno la lettura del tempo dal modulo RTC
+  myRTC.updateTime();
+
+// controllo se ci sono comandi da serialie per rimettere l'orologio
   if (Serial.available()){
-      if (Serial.peek()=='s') {
+      if (Serial.peek()=='s') { // 's' significa che volgio rimettere la data e l'ora. 'q' per annullare l'operazione
         Serial.read();
         Serial.println("Inserisci la data ed il tempo nel");
         Serial.println("formato: aa,mm,gg,gs,hh,mm,ss oppure q per annullare");
         while(!Serial.available());
         delay(20);
-        Serial.println(Serial.available());
-          if(Serial.peek()=='q'||Serial.available()<19) {
-            Serial.read();
+        //Serial.println(Serial.available());
+        // se ho inviato 'q' oppure una stringa non valida di dimensioni inferiori a 19 caratteri allora annullo tutto
+          if(Serial.peek()=='q'||Serial.available()<19) { 
+            Serial.read(); //elimino carattere dal registro di ingresso della seriale
             Serial.println("Dato non valido");
             return;
           }
-        for (int i=0;i<7;i++){ 
+        for (int i=0;i<7;i++){ // estraggo 7 numeri dalla stringa inviata via seriale
           dateTime[i]=Serial.parseInt();
-          Serial.println(dateTime[i]);
+          //Serial.println(dateTime[i]);
         }
+        // adesso aggiorno data ed ora nel modulo RTC
         myRTC.setDS1302Time(dateTime[6],dateTime[5],dateTime[4],dateTime[3],dateTime[2],dateTime[1],dateTime[0]+2000);
       }
-      Serial.read();
+      Serial.read();//elimino carattere dal registro di ingresso della seriale
       } 
 
 
 #ifdef USE_LCD
-  lcd.setCursor(0, 0); // Sposto il cursore nella prima colonna, prima riga
+  lcd.setCursor(0, 0); // Sposto il cursore nella prima colonna, prima riga per stampare la DATA
   lcd.print("Date: ");
   lcd.print(myRTC.year);
   lcd.print("/");
-  if(myRTC.month<10)lcd.print(0);
+  
+  // se il valore da stampare è di una sola cifra stampo 0 e 
+  // poi a seguire stampo la cifra singola per non sporcare il display
+  if(myRTC.month<10)lcd.print(0); 
   lcd.print(myRTC.month);
   lcd.print("/");
   if(myRTC.dayofmonth<10)lcd.print(0);
   lcd.print(myRTC.dayofmonth);
 
-// Sposto il cursore nella prima colonna, seconda riga
+// Sposto il cursore nella prima colonna, seconda riga per stampare l' ORARIO
   lcd.setCursor(0, 1);
   lcd.print("Time: ");
   if(myRTC.hours<10)lcd.print(0); 
@@ -123,7 +118,6 @@ void loop()  {                                                                  
   lcd.print(myRTC.seconds);
 #endif
 
-  
   Serial.print(myRTC.dayofmonth);
   Serial.print("/");
   Serial.print(myRTC.month); 
@@ -137,7 +131,7 @@ void loop()  {                                                                  
   Serial.println(myRTC.seconds);                                                                                                       
   delay( 1000);
 
-}                                                                                                         
+}                                                                                     
 
 
 
